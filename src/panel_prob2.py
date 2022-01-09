5cb4851fc13a00050c26378f5dcc8bbc2b46f84b
an-seunghwan <dpeltms79@gmail.com>
2022-01-09 13:02:22 +0900

    Create panel_prob2.py

---
+++ src/panel_prob2.py
@@ -0,0 +1,57 @@
+#%%
+import pandas as pd
+import numpy as np
+import os
+os.chdir('/Users/anseunghwan/Documents/uos/통계청+skt/전수_인구패널데이터_가구_20220107_78241_데이터')
+#%%
+df2017 = pd.read_csv('./전수_인구패널데이터_가구_2017_20220107_78241.csv', encoding='cp949')
+df2019 = pd.read_csv('./전수_인구패널데이터_가구_2019_20220107_78241.csv', encoding='cp949')
+#%%
+# np.unique(df2017['가구번호'].apply(lambda x: str(x)[:2]))
+# np.unique(df2019['가구번호'].apply(lambda x: str(x)[:2]))
+# df17 = df2017[df2017['가구번호'].apply(lambda x: str(x)[:2]) == '17']
+# df19 = df2019[df2019['가구번호'].apply(lambda x: str(x)[:2]) == '19']
+#%%
+'''only seoul'''
+df17 = df2017[df2017['행정구역시군구코드'].apply(lambda x: str(x)[:2]) == '11']
+# len(np.unique(df17['가구번호']))
+# np.unique(df17['가구번호'].apply(lambda x: str(x)[:2]))
+# df17['가구번호'] = df17['가구번호'].apply(lambda x: str(x)[3:])
+# df17[df17['가구번호'].apply(lambda x: str(x)[:2]) == '15']
+
+df19 = df2019[df2019['행정구역시군구코드'].apply(lambda x: str(x)[:2]) == '11']
+# len(np.unique(df19['가구번호']))
+# np.unique(df19['가구번호'].apply(lambda x: str(x)[:2]))
+# df19['가구번호'] = df19['가구번호'].apply(lambda x: str(x)[3:])
+# df19[df19['가구번호'].apply(lambda x: str(x)[:2]) == '15']
+#%%
+'''select 2017 and 2019 each'''
+'''
+각 기준년도마다 서로 겹치는 가구번호가 존재
+기준년도를 필요한 년도만 추출하고 (2017, 2019)
+기준년도에 존재하는 가구번호를 이용해 병합
+즉, 15-~, 16-~ 등의 번호는 unique한 id임
+'''
+df17 = df17[df17['기준년도'] == 2017]
+df19 = df19[df19['기준년도'] == 2019]
+#%%
+'''가구번호를 기준으로 병합'''
+df = pd.merge(df17[['가구번호', '가구원수']], df19[['가구번호', '가구원수']], how='inner', on='가구번호')
+#%%
+'''marginal prob'''
+result = {}
+for idx in range(1, 4): # idx = 가구원수
+    if idx != 3:
+        df['가구원수_x{}'.format(idx)] = df['가구원수_x'].apply(lambda x: 0 if x != idx else 1)
+        df['가구원수_y{}'.format(idx)] = df['가구원수_y'].apply(lambda x: 0 if x != idx else 1)
+    else:
+        df['가구원수_x{}'.format(idx)] = df['가구원수_x'].apply(lambda x: 0 if x < idx else 1)
+        df['가구원수_y{}'.format(idx)] = df['가구원수_y'].apply(lambda x: 0 if x < idx else 1)
+
+    probs = np.zeros((2, 2))
+    for i in range(2):
+        for j in range(2):
+            probs[i, j] = ((df['가구원수_x{}'.format(idx)] == i).astype(float) * (df['가구원수_y{}'.format(idx)] == j).astype(float)).sum() / df.shape[0]
+    result[idx] = probs
+result
+#%%
