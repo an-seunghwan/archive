3ed16882f16c74ad539e24d8219358f61567f991
an-seunghwan <dpeltms79@gmail.com>
2022-01-10 15:54:32 +0900

    Update panel_prob4.py

--- src/panel_prob4.py
+++ src/panel_prob4.py
@@ -45,8 +45,6 @@ B[1, 0] = p2 + p3 - 1
 alpha = (np.linalg.inv(A) @ B)[0, 0]
 beta = (np.linalg.inv(A) @ B)[1, 0]
 #%%
-
-#%%
 # transition = np.zeros((2, 2))
 # A = np.zeros((2, 2))
 # B = np.zeros((2, 1))
@@ -63,4 +61,51 @@ beta = (np.linalg.inv(A) @ B)[1, 0]
 # B[1, 0] = p2 + p3 - 1
 
 # np.linalg.inv(A) @ B
+#%%
+# Import the necessary packages
+from cvxopt import matrix
+from cvxopt import solvers
+
+# Define QP parameters (with NumPy)
+P = matrix(2 * np.array([[p1**2 + p2**2, 2 * p1 * (p1 - 1)],
+                     [2 * p2 * (p2 - 1), (p1 - 1) ** 2 + (p2 - 1) ** 2]]), tc='d')
+q = matrix(np.array([2 * p1 * (1 - p1 - p2) + 2 * p2 * (1 - p2 - p3),
+                     2 * (p1 - 1) * (1 - p1 - p2) + 2 * (p2 - 1) * (1 - p2 - p3)]), tc='d')
+G = matrix(np.array([[1, 0],
+                     [0, 1],
+                     [-1, 0],
+                     [0, -1]]), tc='d')
+h = matrix(np.array([1, 1, 0, 0]), tc='d')
+
+# Construct the QP, invoke solver
+sol = solvers.qp(P,q,G,h)
+
+# Extract optimal value and solution
+np.array(sol['x'])
+
+np.array(sol['primal objective'] )
+#%%
+for idx in range(1, 4):
+    # idx = 1
+    A = np.zeros((2, 2))
+    B = np.zeros((2, 1))
+
+    pi1 = (gagu2017['가구원수'][gagu2017['가구원수'] == idx].sum() + 
+        gagu2018['가구원수'][gagu2018['가구원수'] == idx].sum() + 
+        gagu2019['가구원수'][gagu2019['가구원수'] == idx].sum()) / (gagu2017['가구원수'].sum() + gagu2018['가구원수'].sum() + gagu2019['가구원수'].sum())
+
+    p1 = gagu2017['가구원수'][gagu2017['가구원수'] == idx].sum() / gagu2017['가구원수'].sum()
+    p3 = gagu2019['가구원수'][gagu2019['가구원수'] == idx].sum() / gagu2019['가구원수'].sum()
+
+    A[0, 0] = pi1
+    A[0, 1] = 1 - pi1 
+    A[1, 0] = p1
+    A[1, 1] = p1 - 1
+
+    B[0, 0] = pi1
+    B[1, 0] = p1 + p3 - 1
+
+    alpha = (np.linalg.inv(A) @ B)[0, 0]
+    beta = (np.linalg.inv(A) @ B)[1, 0]
+    print(alpha, beta)
 #%%
