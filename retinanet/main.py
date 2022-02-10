456a9ab27b0aeaea307ebedc87f31e0089c0c165
an-seunghwan <dpeltms79@gmail.com>
2022-02-10 20:45:19 +0900

    update

---
+++ retinanet/main.py
@@ -0,0 +1,301 @@
+#%%
+"""
+Title: Object Detection with RetinaNet
+Author: [Srihari Humbarwadi](https://twitter.com/srihari_rh)
+Date created: 2020/05/17
+Last modified: 2020/07/14
+Description: Implementing RetinaNet: Focal Loss for Dense Object Detection.
+Modified version
+"""
+
+"""
+## Introduction
+
+Object detection a very important problem in computer
+vision. Here the model is tasked with localizing the objects present in an
+image, and at the same time, classifying them into different categories.
+Object detection models can be broadly classified into "single-stage" and
+"two-stage" detectors. Two-stage detectors are often more accurate but at the
+cost of being slower. Here in this example, we will implement RetinaNet,
+a popular single-stage detector, which is accurate and runs fast.
+RetinaNet uses a feature pyramid network to efficiently detect objects at
+multiple scales and introduces a new loss, the Focal loss function, to alleviate
+the problem of the extreme foreground-background class imbalance.
+
+**References:**
+
+- [RetinaNet Paper](https://arxiv.org/abs/1708.02002)
+- [Feature Pyramid Network Paper](https://arxiv.org/abs/1612.03144)
+"""
+#%%
+import os
+os.chdir('/Users/anseunghwan/Documents/GitHub/archive/retinanet')
+import re
+import zipfile
+
+import numpy as np
+import tensorflow as tf
+import tensorflow.keras as K
+from tqdm import tqdm
+
+import matplotlib.pyplot as plt
+import tensorflow_datasets as tfds
+
+import datetime
+current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
+
+from utils import *
+from preprocess import *
+from anchors import *
+from model import *
+from labeling import *
+#%%
+batch_size = 2
+num_classes = 20
+model_dir = "model/"
+epochs = 320
+#%%
+class_dict = {
+    'aeroplane': 1,
+    'bicycle': 2,
+    'bird': 3,
+    'boat': 4,
+    'bottle': 5,
+    'bus': 6,
+    'car': 7,
+    'cat': 8,
+    'chair': 9,
+    'cow': 10,
+    'diningtable': 11,
+    'dog': 12,
+    'horse': 13,
+    'motorbike': 14,
+    'person': 15,
+    'pottedplant': 16,
+    'sheep': 17,
+    'sofa': 18,
+    'train': 19,
+    'tvmonitor': 20
+}
+class_dict = {x:y-1 for x,y in class_dict.items()}
+classnum_dict = {y:x for x,y in class_dict.items()}
+#%%
+# '''dataset'''
+# train_dataset, val_dataset, test_dataset, ds_info = fetch_dataset(batch_size)
+# train_dataset = train_dataset.concatenate(val_dataset)
+
+# log_path = f'logs/voc2007'
+
+# """
+# visualization
+# """
+# image, bbox, label = next(iter(train_dataset))
+# image = np.array(image[0], dtype=np.uint8)
+# bbox = bbox[0].numpy()
+# label = label[0].numpy()
+# img_height, img_width, _ = image.shape
+
+# plt.figure(figsize=(7, 7))
+# # plt.axis("off")
+# plt.imshow(image)
+# ax = plt.gca()
+# for box, _cls in zip(bbox, label):
+#     text = "{}".format(classnum_dict.get(_cls))
+#     x, y, w, h = box.tolist()
+#     # y_min, x_min = round(y_min * img_height), round(x_min * img_width)
+#     # y_max, x_max = round(y_max * img_height), round(x_max * img_width)
+#     patch = plt.Rectangle(
+#         [x - w/2, y - h/2], w, h, fill=False, edgecolor='red', linewidth=2
+#     )
+#     ax.add_patch(patch)
+#     ax.text(
+#         x - w/2,
+#         y - h/2,
+#         text,
+#         bbox={"facecolor": 'blue', "alpha": 0.4},
+#         clip_box=ax.clipbox,
+#         clip_on=True,
+#     )
+# plt.show()
+#%%
+'''dataset'''
+train_dataset, val_dataset, test_dataset, ds_info = fetch_dataset(batch_size)
+train_dataset = train_dataset.concatenate(val_dataset)
+
+log_path = f'logs/voc2007'
+
+"""
+## Initializing and compiling model
+"""
+resnet50_backbone = get_backbone()
+model = RetinaNet(num_classes, resnet50_backbone)
+model.build(input_shape=[None, 512, 512, 3])
+# loss_fn = RetinaNetLoss(num_classes)
+
+learning_rates = [2.5e-06, 0.000625, 0.00125, 0.0025, 0.00025, 2.5e-05]
+learning_rate_boundaries = [125, 250, 500, 240000, 360000]
+
+optimizer = K.optimizers.SGD(learning_rate=learning_rates[0], momentum=0.9)
+model.summary()
+
+label_encoder = LabelEncoder()
+
+train_writer = tf.summary.create_file_writer(f'{log_path}/{current_time}/train')
+val_writer = tf.summary.create_file_writer(f'{log_path}/{current_time}/val')
+test_writer = tf.summary.create_file_writer(f'{log_path}/{current_time}/test')
+
+# total_length = sum(1 for _ in train_dataset)
+iteration = (ds_info.splits["train"].num_examples + ds_info.splits["validation"].num_examples) // batch_size
+#%%
+for epoch in range(epochs):
+    loss_avg = tf.keras.metrics.Mean()
+    loss_box = tf.keras.metrics.Mean()
+    loss_clf = tf.keras.metrics.Mean()
+    accuracy = tf.keras.metrics.SparseCategoricalAccuracy()
+    
+    iter_ = iter(train_dataset)
+    
+    progress_bar = tqdm.tqdm(range(iteration), unit='batch')
+    for batch_num in progress_bar:
+        
+        '''learning rate schedule'''
+        epoch_ = epoch * iteration + batch_num
+        if epoch_ < learning_rate_boundaries[0]: optimizer.lr = learning_rates[0]
+        elif epoch_ < learning_rate_boundaries[1]: optimizer.lr = learning_rates[1]
+        elif epoch_ < learning_rate_boundaries[2]: optimizer.lr = learning_rates[2]
+        elif epoch_ < learning_rate_boundaries[3]: optimizer.lr = learning_rates[3]
+        elif epoch_ < learning_rate_boundaries[4]: optimizer.lr = learning_rates[4]
+        else: optimizer.lr = learning_rates[5]
+
+        image, bbox, label = next(iter_)
+        image, bbox_true, cls_true = label_encoder.encode_batch(image, bbox, label)        
+
+        with tf.GradientTape(persistent=True) as tape:
+            bbox_pred, cls_pred = model(image)
+            loss, box_loss, clf_loss = RetinaNetLoss(bbox_true, cls_true, bbox_pred, cls_pred)
+                        
+        grads = tape.gradient(loss, model.trainable_variables) 
+        optimizer.apply_gradients(zip(grads, model.trainable_variables)) 
+        
+        accuracy(cls_true, cls_pred)
+        
+        progress_bar.set_postfix({
+            'EPOCH': f'{epoch:04d}',
+            'Loss': f'{loss_avg.result():.4f}',
+            'Box': f'{loss_box.result():.4f}',
+            'CLS': f'{loss_clf.result():.4f}',
+            'Accuracy': f'{accuracy.result():.3%}'
+        })
+#%%
+"""
+save model
+"""
+model_path = f'{log_path}/{current_time}'
+if not os.path.exists(model_path):
+    os.makedirs(model_path)
+model.save_weights(model_path + '/model_{}.h5'.format(current_time), save_format="h5")
+#%%
+# """
+# ## Implementing a custom layer to decode predictions
+# """
+
+# class DecodePredictions(tf.keras.layers.Layer):
+#     """A Keras layer that decodes predictions of the RetinaNet model.
+
+#     Attributes:
+#       num_classes: Number of classes in the dataset
+#       confidence_threshold: Minimum class probability, below which detections
+#         are pruned.
+#       nms_iou_threshold: IOU threshold for the NMS operation
+#       max_detections_per_class: Maximum number of detections to retain per
+#        class.
+#       max_detections: Maximum number of detections to retain across all
+#         classes.
+#       box_variance: The scaling factors used to scale the bounding box
+#         predictions.
+#     """
+
+#     def __init__(
+#         self,
+#         num_classes=20,
+#         confidence_threshold=0.05,
+#         nms_iou_threshold=0.5,
+#         max_detections_per_class=100,
+#         max_detections=100,
+#         box_variance=[0.1, 0.1, 0.2, 0.2],
+#         **kwargs
+#     ):
+#         super(DecodePredictions, self).__init__(**kwargs)
+#         self.num_classes = num_classes
+#         self.confidence_threshold = confidence_threshold
+#         self.nms_iou_threshold = nms_iou_threshold
+#         self.max_detections_per_class = max_detections_per_class
+#         self.max_detections = max_detections
+
+#         self._anchor_box = AnchorBox()
+#         self._box_variance = tf.convert_to_tensor(
+#             [0.1, 0.1, 0.2, 0.2], dtype=tf.float32
+#         )
+
+#     def _decode_box_predictions(self, anchor_boxes, box_predictions):
+#         boxes = box_predictions * self._box_variance
+#         boxes = tf.concat(
+#             [
+#                 boxes[:, :, :2] * anchor_boxes[:, :, 2:] + anchor_boxes[:, :, :2],
+#                 tf.math.exp(boxes[:, :, 2:]) * anchor_boxes[:, :, 2:],
+#             ],
+#             axis=-1,
+#         )
+#         boxes_transformed = convert_to_corners(boxes)
+#         return boxes_transformed
+
+#     def call(self, images, box_pred, cls_pred):
+#         image_shape = tf.cast(tf.shape(images), dtype=tf.float32)
+#         anchor_boxes = self._anchor_box.get_anchors(image_shape[1], image_shape[2])
+#         cls_predictions = tf.nn.sigmoid(cls_pred)
+#         boxes = self._decode_box_predictions(anchor_boxes[None, ...], box_pred)
+
+#         return tf.image.combined_non_max_suppression(
+#             tf.expand_dims(boxes, axis=2),
+#             cls_predictions,
+#             self.max_detections_per_class,
+#             self.max_detections,
+#             self.nms_iou_threshold,
+#             self.confidence_threshold,
+#             clip_boxes=False,
+#         )
+# #%%
+# """
+# ## Building inference model
+# """
+# image = tf.keras.Input(shape=[512, 512, 3], name="image")
+# predictions = model(image, training=False)
+# detections = DecodePredictions(confidence_threshold=0.5)(image, predictions[0], predictions[1])
+# inference_model = tf.keras.Model(inputs=image, outputs=detections)
+# #%%
+# """
+# ## Generating detections
+# """
+# def prepare_image(image):
+#     ratio = 512 / tf.reduce_max(tf.shape(image)[:2])
+#     image_shape = [512, 512]
+#     image_shape = tf.cast(image_shape, dtype=tf.float32)
+#     image = tf.image.resize(image, tf.cast(image_shape, dtype=tf.int32))
+#     image = tf.keras.applications.resnet.preprocess_input(image)
+#     return tf.expand_dims(image, axis=0), ratio
+# #%%
+# for sample in test_dataset.take(2):
+#     image = tf.cast(sample[0], dtype=tf.float32)
+#     input_image, ratio = prepare_image(image[0])
+#     detections = inference_model.predict(input_image)
+#     num_detections = detections.valid_detections[0]
+#     class_names = [
+#         classnum_dict(int(x)) for x in detections.nmsed_classes[0][:num_detections]
+#     ]
+#     visualize_detections(
+#         image[0],
+#         detections.nmsed_boxes[0][:num_detections] / ratio,
+#         class_names,
+#         detections.nmsed_scores[0][:num_detections],
+#     )
+#%%
---
+++ retinanet/utils.py
@@ -0,0 +1,90 @@
+#%%
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+#%%
+"""
+## Implementing utility functions
+
+Bounding boxes can be represented in multiple ways, the most common formats are:
+
+- Storing the coordinates of the corners `[xmin, ymin, xmax, ymax]`
+- Storing the coordinates of the center and the box dimensions
+`[x, y, width, height]`
+
+Since we require both formats, we will be implementing functions for converting
+between the formats.
+"""
+
+def swap_xy(boxes):
+    """Swaps order the of x and y coordinates of the boxes.
+
+    Arguments:
+        boxes: A tensor with shape `(num_boxes, 4)` representing bounding boxes.
+
+    Returns:
+        swapped boxes with shape same as that of boxes.
+    """
+    return tf.stack([boxes[:, 1], boxes[:, 0], boxes[:, 3], boxes[:, 2]], axis=-1)
+
+def convert_to_xywh(boxes):
+    """Changes the box format to center, width and height.
+
+    Arguments:
+        boxes: A tensor of rank 2 or higher with a shape of `(..., num_boxes, 4)`
+        representing bounding boxes where each box is of the format
+        `[xmin, ymin, xmax, ymax]`.
+
+    Returns:
+        converted boxes with shape same as that of boxes.
+    """
+    return tf.concat(
+        [(boxes[..., :2] + boxes[..., 2:]) / 2.0, boxes[..., 2:] - boxes[..., :2]],
+        axis=-1,
+    )
+
+def convert_to_corners(boxes):
+    """Changes the box format to corner coordinates
+
+    Arguments:
+        boxes: A tensor of rank 2 or higher with a shape of `(..., num_boxes, 4)`
+        representing bounding boxes where each box is of the format
+        `[x, y, width, height]`.
+
+    Returns:
+        converted boxes with shape same as that of boxes.
+    """
+    return tf.concat(
+        [boxes[..., :2] - boxes[..., 2:] / 2.0, boxes[..., :2] + boxes[..., 2:] / 2.0],
+        axis=-1,
+    )
+#%%
+'''FIXME'''
+def visualize_detections(
+    image, boxes, classes, scores, figsize=(7, 7), linewidth=1, color=[0, 0, 1]
+):
+    """Visualize Detections"""
+    image = np.array(image, dtype=np.uint8)
+    plt.figure(figsize=figsize)
+    plt.axis("off")
+    plt.imshow(image)
+    ax = plt.gca()
+    for box, _cls, score in zip(boxes, classes, scores):
+        text = "{}: {:.2f}".format(_cls, score)
+        x1, y1, x2, y2 = box
+        w, h = x2 - x1, y2 - y1
+        patch = plt.Rectangle(
+            [x1, y1], w, h, fill=False, edgecolor=color, linewidth=linewidth
+        )
+        ax.add_patch(patch)
+        ax.text(
+            x1,
+            y1,
+            text,
+            bbox={"facecolor": color, "alpha": 0.4},
+            clip_box=ax.clipbox,
+            clip_on=True,
+        )
+    plt.show()
+    return ax
+#%%
